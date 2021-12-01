ef0494f21ef5c75122678415d0659aa30bde0e76
Dominik Mydlil <dominik.mydlil@outlook.com>
2021-12-01 19:32:36 +0100

    complete day 1

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let input = include_str!("input.txt");
+    let nums: Vec<u32> = input.lines().map(|l| l.parse::<u32>().unwrap()).collect();
+
+    println!("simple increase: {}", count_simple_incr(&nums));
+    println!("sliding window: {}", count_sliding_window(&nums));
+}
+
+fn count_sliding_window(nums: &[u32]) -> i32 {
+    let nums: Vec<u32> = nums.windows(3).map(|w| w.iter().sum()).collect();
+    count_incr(&nums)
+}
+
+fn count_simple_incr(nums: &[u32]) -> i32 {
+    count_incr(nums)
+}
+
+fn count_incr(nums: &[u32]) -> i32 {
+    if nums.len() <= 1 {
+        return 0;
+    }
+
+    let mut r = nums[0];
+    let mut c = 0;
+    for n in nums[1..].iter() {
+        if *n > r {
+            c += 1;
+        }
+        r = *n;
+    }
+
+    c
+}
