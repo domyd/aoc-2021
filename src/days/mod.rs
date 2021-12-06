eb64571f07e0ca63c8a8eb7c88d40c5542e672cd
Dominik Mydlil <dominik.mydlil@outlook.com>
2021-12-06 11:43:46 +0100

    complete day 6

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -3,9 +3,11 @@ mod four;
 mod one;
 mod three;
 mod two;
+mod six;
 
 pub use five::five;
 pub use four::four;
 pub use one::one;
 pub use three::three;
 pub use two::two;
+pub use six::six;
---
+++ src/days/six.rs
@@ -0,0 +1,23 @@
+pub fn six() {
+    let input = include_str!("../inputs/six.txt");
+    let lines: Vec<&str> = input.lines().collect();
+    let input: Vec<usize> = lines[0]
+        .split(',')
+        .filter_map(|x| x.parse::<usize>().ok())
+        .collect();
+    let mut fish: [usize; 9] = [0; 9];
+
+    for &i in input.iter() {
+        fish[i] += 1;
+    }
+
+    for day in 1..=256 {
+        let ready_fish = fish[0];
+        fish[0] = 0;
+        fish.rotate_left(1);
+        fish[6] += ready_fish;
+        fish[8] += ready_fish;
+        let count: usize = fish.iter().sum();
+        println!("after {} days: {} fish", day, count);
+    }
+}
